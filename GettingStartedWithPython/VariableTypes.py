e018a7f52d635b257ec1f666c150869dfa376695
Kuldeep Chaturvedi <kuldeepchaturvedi475@gmail.com>
2019-05-18 23:34:03 +0530

    bsic syntax and variables

---
+++ GettingStartedWithPython/VariableTypes.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+emp_Id      = 101        # Integer type 
+emp_Name    = 'Bob'      # String type
+emp_Salary  = 22000.54   # Float type
+emp_Is_Deleted = False   # Bollen type
---
+++ GettingStartedWithPython/basic_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+#Lets try with printing hello world in python
+
+# Method-1
+print('hello world!')
+
+# Method-2
+print("hello world!")
+
