4eebc468005c7d5984648716c247af02e85b08d0
Kuldeep Chaturvedi <kuldeepchaturvedi475@gmail.com>
2019-05-27 00:46:27 +0530

    Day 9

--- Python For Data Science/twitter_sentiment_analysis.py
+++ Python For Data Science/twitter_sentiment_analysis.py
@@ -1,35 +1,66 @@
+# Python script to generate csv file which contains tweet and its polartiy with subjectivity and objectivity of tweet.
 import tweepy
 from textblob import TextBlob
 import csv
+import datetime
 
-consumer_key = 'kqyT3fp8fTt3JC2dwnVuVze8J'
-consumer_secert_key = 'jLzLV2vliLYfqJu29yllG2axSdDaYujeIAMvoBe0heWxhibhsH'
+consumer_key = None
+consumer_secert_key = None
+access_token = None
+access_token_secret = None
+topic = None
+while(consumer_key == None or consumer_key == ''):
+    consumer_key = input('Enter the consumer Key = ')
+while(consumer_secert_key == None or consumer_secert_key == ''):
+    consumer_secert_key = input('Enter the consumer secert Key = ')
+while(access_token == None or access_token == ''):
+    access_token = input('Enter access token = ')
+while(access_token_secret == None or access_token_secret == ''):
+    access_token_secret = input('Enter access token secret = ')
 
-access_token = '404176458-EcOc10GVeQjkyPDW6xHZJG9e1Q9o1cYugBuxH3Mz'
-access_token_secret = 'f3ilUWddcQV27Q5NAZ7qSkiGg4Vq8WOvoalVDLYzZmMb7'
-
+print('--------------------'*4)
+print('Please wait Authenticating....')
 auth = tweepy.OAuthHandler(consumer_key,consumer_secert_key)
+print('--------------------'*4)
+print('Please wait setting access token....')
 auth.set_access_token(access_token,access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.search('RedmiNote7Pro')
+while(topic == None or topic == ''):
+    topic = input('Please enter keyword to search = ')
+print('--------------------'*4)
+print('Fetching twittes......')
+public_tweets = api.search(q=topic,count=100)
 
-csvData = [['Tweet','Polarity','Subjectivity']]
-
+csvData = [['Tweet','Polarity','Subjectivity','Subective / Objective']]
 for tweet in public_tweets:
-    print('-------------'*4)
-    print(tweet.text)
+    tweet_text = tweet.text
     analysis = TextBlob(tweet.text)
-    print('------------- Sentiment Polarity -------------')
-    print(analysis.sentiment.polarity)
-    print('------------- Sentiment Subjectivity -------------')
-    print(analysis.sentiment.subjectivity)
-    csvData.append([tweet.text,"{:.2f}".format(analysis.sentiment.polarity),"{:.2f}".format(analysis.sentiment.subjectivity)])
+    polarity_score = analysis.sentiment.polarity
+    subjectivity_score = analysis.sentiment.subjectivity
+    subjective_or_objective = None
+    if subjectivity_score > 0.5:
+        if subjectivity_score == 1.0:
+            subjective_or_objective = 'Very Subjective'
+        else:
+            subjective_or_objective = 'Subjective'
+    if subjectivity_score < 0.5:
+        if subjectivity_score == 0.0:
+            subjective_or_objective = 'Very Objective'
+        else:
+            subjective_or_objective = 'Objective'
+    csvData.append([tweet.text,polarity_score,subjectivity_score,subjective_or_objective])
 
+current_date_time  = datetime.datetime.now().strftime('%d%m%y_%H%M%S')
 
-with open('twitter.csv', 'w', encoding='utf-8') as csvFile:
+filename = topic + '_' + str(current_date_time) + '.csv'
+print('--------------------'*4)
+print('Creating File ' + filename)
+print('Writing in file ' + filename)
+with open(filename, 'w', encoding='utf-8') as csvFile:
     writer = csv.writer(csvFile)
     writer.writerows(csvData)
+print(filename + ' created successfully.')
 
 csvFile.close()
